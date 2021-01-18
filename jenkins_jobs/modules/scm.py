7edfc44fcea986d4df916ed605638d0dd3a1eb7d
Unpublished <unpublished@gmx.net>
2021-01-18 18:55:14 +0100

    Avoid MissingFieldException for folders

--- jenkins_jobs/modules/scm.py
+++ jenkins_jobs/modules/scm.py
@@ -1711,6 +1711,7 @@ class SCM(jenkins_jobs.modules.base.Base):
 
         # multibranch-pipeline scm implementation is incompatible with SCM
         if data.get("project-type") in [
+            "folder",
             "multibranch",
             "multibranch-defaults",
             "pipeline",
